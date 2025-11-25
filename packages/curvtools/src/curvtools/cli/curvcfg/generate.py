0657eb8860141ff631206678dddd2d2a67af0867
Mike Goelzer <mikegoelzer@goelzer.org>
2025-11-25 06:59:38 -0700

    simplify generate

--- packages/curvtools/src/curvtools/cli/curvcfg/generate.py
+++ packages/curvtools/src/curvtools/cli/curvcfg/generate.py
@@ -3,12 +3,11 @@ import os
 from typing import Optional, Dict, Union
 from pathlib import Path
 from curvtools.cli.curvcfg.lib.globals.console import console
-from curvtools.cli.curvcfg.lib.globals.curvpaths import get_curv_paths
-from curvpyutils.toml_utils import MergedTomlDict  # type: ignore
+from curvpyutils.toml_utils import MergedTomlDict
 from curvtools.cli.curvcfg.lib.util import get_config_values, emit_config_files
 from curvtools.cli.curvcfg.lib.globals.types import CurvCliArgs
 
-def generate(args: CurvCliArgs) -> int:
+def generate(args: CurvCliArgs, ctx_obj: dict) -> int:
     """
     Generate output files from a merged config TOML and a schema.
 
@@ -18,10 +17,11 @@ def generate(args: CurvCliArgs) -> int:
     Returns:
         Exit code
     """
-    get_curv_paths(args)
+    curv_paths: CurvPaths = ctx_obj.get("CurvPaths")
+    assert curv_paths is not None, "CurvPaths not found in context object"
 
     # Resolve inputs
-    merged_toml_path = args.get("merged_file")
+    merged_toml_path = str(args.get("merged_file"))
     
 
     # Validate readable input
@@ -32,8 +32,8 @@ def generate(args: CurvCliArgs) -> int:
     verbosity = int(args.get("verbosity", 0) or 0)
 
     try:
-        merged = MergedTomlDict.from_toml_files(merged_toml_path)
-        outdir_path = Path(args.get("build_dir")) / "generated"
+        merged = MergedTomlDict(merged_toml_path)
+        outdir_path = args.get("build_dir") / "generated"
     except Exception as exc:
         console.print(f"[red]error:[/red] failed reading merged config: {exc}")
         return 1
@@ -41,24 +41,23 @@ def generate(args: CurvCliArgs) -> int:
     # If merged already has schema information in it, then we don't need the separate schema file and will ignore it.
     if '_schema' not in merged.keys():
         # we need the file, so get it and validate it
-        schema_file_path = args.get("schema_file")
-        if not (os.path.isfile(schema_file_path) and os.access(schema_file_path, os.R_OK)):
-            console.print(f"[red]error:[/red] schema file not found or unreadable: {schema_file_path}")
-            return 1
+        schema_file_path_list = args.get("schema_file_list")
+        assert all(isinstance(schema_file_path, (str, Path)) for schema_file_path in schema_file_path_list), "all schema file paths must be strings or Path objects"
+        assert all(os.path.isfile(schema_file_path) and os.access(schema_file_path, os.R_OK) for schema_file_path in schema_file_path_list), "all schema files must be found and readable"
         is_combined_toml = False
     else:
-        schema_file_path = None
+        schema_file_path_list = None
         is_combined_toml = True
     
     # Collect values and emit
     try:
-        cfg_values = get_config_values(merged, schema_file_path, is_combined_toml=is_combined_toml)
+        cfg_values = get_config_values(merged, schema_file_path_list, is_combined_toml=is_combined_toml)
         files_emitted, files_unchanged = emit_config_files(cfg_values, outdir_path=outdir_path, verbosity=verbosity)
         if verbosity == 1:
             for file in files_emitted:
-                console.print(f"[green]emitted:[/green] {get_curv_paths().mk_rel_to_cwd(file)}")
+                console.print(f"[green]emitted:[/green] {CurvPaths.mk_rel_to_cwd(file)}")
             for file in files_unchanged:
-                console.print(f"[yellow]unchanged:[/yellow] {get_curv_paths().mk_rel_to_cwd(file)}")
+                console.print(f"[yellow]unchanged:[/yellow] {CurvPaths.mk_rel_to_cwd(file)}")
     except SystemExit:
         raise
     except Exception as exc:
